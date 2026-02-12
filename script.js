457b1ecd258e11e76b60c39d8e2374783d663dc3
victoria.calvo1013@hotmail.com <victoria.calvo1013@hotmail.com>
2026-02-12 17:59:55 -0500

    update styles and add js

--- script.js
+++ script.js
@@ -10,13 +10,20 @@ function validateEmail(email){
     document.getElementById('signupForm').addEventListener('submit', function(event) {
     event.preventDefault(); //prevents form from being submitted
     const errorMessage = document.getElementById('error-message'); //declare var for error message to be displayed
+    const errorStyle = document.querySelector(".error");
+    const errorImg = document.querySelector(".error-icon");
 
     //check the user's input to display success or failure alert
     if (validateEmail(emailInput.value)) {
         errorMessage.textContext = ""; //clears any previous error message
         alert('Thank you for subscribing!');
     } else {
-        errorMessage.textContent = "Please enter a valid email address"
+        errorMessage.textContent = "Please provide a valid email";
+        errorStyle.style.display ="flex";
+        emailInput.style.borderColor = "red";
+        errorImg.innerHTML = `
+        <img src = "images/icon-error.svg" alt="error icon">
+    `
     }
 });
 
