60007e632d17425a129c2f9441949023adab02a4
Victoria <victoria.calvo1013@hotmail.com>
2026-02-22 00:07:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,20 +10,15 @@ function validateEmail(email){
     document.getElementById('signupForm').addEventListener('submit', function(event) {
     event.preventDefault(); //prevents form from being submitted
     const errorMessage = document.getElementById('error-message'); //declare var for error message to be displayed
-    const errorStyle = document.querySelector(".error");
-    const errorImg = document.querySelector(".error-icon");
+    const errorIcon = document.querySelector(".error-icon");
 
     //check the user's input to display success or failure alert
     if (validateEmail(emailInput.value)) {
-        errorMessage.textContext = ""; //clears any previous error message
+        errorMessage.textContent = ""; //clears any previous error message
         alert('Thank you for subscribing!');
     } else {
-        errorMessage.textContent = "Please provide a valid email";
-        errorStyle.style.display ="flex";
-        emailInput.style.borderColor = "red";
-        errorImg.innerHTML = `
-        <img src = "images/icon-error.svg" alt="error icon">
-    `
+        errorMessage.style.display="block";
+        errorIcon.style.display="block";
     }
 });
 
